c9111016cdab3d2888f715179225b1e404eca93d
LixvYang <2690688423@qq.com>
2025-01-05 23:18:40 +0800

    update

--- src/types/mixin.d.ts
+++
@@ -1,7 +0,0 @@
-// import { WebviewAsset } from '@mixin.dev/mixin-node-sdk';
-
-// declare global {
-//   interface Window {
-//     assetsCallbackFunction?: ((assets: string) => void) | undefined;
-//   }
-// }
